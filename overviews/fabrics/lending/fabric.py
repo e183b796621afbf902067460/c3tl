c22adf924e912754564e0d7c0d0f9661ef5e8886
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-08-22 00:33:05 +0300

    Added Aave overview to fabric

--- overviews/fabrics/lending/fabric.py
+++ overviews/fabrics/lending/fabric.py
@@ -1,8 +1,12 @@
 from overviews.fabrics.dex.fabric import DEXOverviewFabric
 
+from overviews.protocols.aave.overview import AaveV2LendingPoolOverview
+
 
 class LendingOverviewFabric(DEXOverviewFabric):
     pass
 
 
 lendingOverviewFabric = LendingOverviewFabric()
+
+lendingOverviewFabric.addProduct(protocol='aave', overview=AaveV2LendingPoolOverview())
