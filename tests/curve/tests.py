ef753d38f579042d0c346ea02cba0f0f8f3431ee
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-09-09 18:48:05 +0300

    Changed test for CurveLiquidityPoolOverview

--- tests/curve/tests.py
+++ tests/curve/tests.py
@@ -1,30 +1,59 @@
-from providers.abstracts.fabric import providerAbstractFabric
+import unittest
+from concurrent.futures import Future
+
+import builtins
+
+from overviews.protocols.curve.overview import CurveLiquidityPoolOverview
 from overviews.abstracts.fabric import overviewAbstractFabric
+
+from providers.abstracts.fabric import providerAbstractFabric
 from traders.head.trader import headTrader
 from head.bridge.configurator import BridgeConfigurator
 
 
-provider = BridgeConfigurator(
-    abstractFabric=providerAbstractFabric,
-    fabricKey='http',
-    productKey='eth')\
-    .produceProduct()
+class TestCurveLiquidityPoolOverview(unittest.TestCase):
 
-overviewContract = BridgeConfigurator(
-    abstractFabric=overviewAbstractFabric,
-    fabricKey='liquidity-pool-overview',
-    productKey='curve')\
-    .produceProduct()()\
-    .setAddress(address='0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7')\
-    .setProvider(provider=provider)\
-    .setTrader(trader=headTrader)\
-    .create()
+    _address = '0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7'
 
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='eth') \
+        .produceProduct()
 
-future = overviewContract.getOverview()
-overview = future.result()
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='liquidity-pool-overview',
+        productKey='curve') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
 
-for aOverview in overview:
-    assert isinstance(aOverview['symbol'], str)
-    assert isinstance(aOverview['reserve'], (int, float))
-    assert isinstance(aOverview['price'], (int, float))
+    def testInstance(self):
+        self.assertIsInstance(self._instance, CurveLiquidityPoolOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview()
+        self.assertIsInstance(future, Future)
+
+        overview = future.result()
+        self.assertIsInstance(overview, list)
+
+        for aOverview in overview:
+            self.assertIsInstance(aOverview, dict)
+
+            self.assertIsInstance(aOverview['symbol'], str)
+            self.assertIsInstance(aOverview['reserve'], (int, float))
+            self.assertIsInstance(aOverview['price'], (int, float))
+        builtins.print('\n', overview)
