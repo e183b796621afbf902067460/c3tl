e497fbdefe7a07c84e3879351f800b9154310a24
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-09-10 13:38:10 +0300

    Changed test for TestEllipsisLiquidityPoolOverview

--- tests/ellipsis/tests.py
+++ tests/ellipsis/tests.py
@@ -1,30 +1,59 @@
-from providers.abstracts.fabric import providerAbstractFabric
+import unittest
+from concurrent.futures import Future
+
+import builtins
+
+from overviews.protocols.ellipsis.overview import EllipsisLiquidityPoolOverview
 from overviews.abstracts.fabric import overviewAbstractFabric
+
+from providers.abstracts.fabric import providerAbstractFabric
 from traders.head.trader import headTrader
 from head.bridge.configurator import BridgeConfigurator
 
 
-provider = BridgeConfigurator(
-    abstractFabric=providerAbstractFabric,
-    fabricKey='http',
-    productKey='bsc')\
-    .produceProduct()
+class TestEllipsisLiquidityPoolOverview(unittest.TestCase):
 
-overviewContract = BridgeConfigurator(
-    abstractFabric=overviewAbstractFabric,
-    fabricKey='liquidity-pool-overview',
-    productKey='ellipsis')\
-    .produceProduct()()\
-    .setAddress(address='0x160CAed03795365F3A589f10C379FfA7d75d4E76')\
-    .setProvider(provider=provider)\
-    .setTrader(trader=headTrader)\
-    .create()
+    _address = '0x160CAed03795365F3A589f10C379FfA7d75d4E76'
 
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='bsc') \
+        .produceProduct()
 
-future = overviewContract.getOverview()
-overview = future.result()
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='liquidity-pool-overview',
+        productKey='ellipsis') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
 
-for aOverview in overview:
-    assert isinstance(aOverview['symbol'], str)
-    assert isinstance(aOverview['reserve'], (int, float))
-    assert isinstance(aOverview['price'], (int, float))
+    def testInstance(self):
+        self.assertIsInstance(self._instance, EllipsisLiquidityPoolOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview()
+        self.assertIsInstance(future, Future)
+
+        overview = future.result()
+        self.assertIsInstance(overview, list)
+
+        for aOverview in overview:
+            self.assertIsInstance(aOverview, dict)
+
+            self.assertIsInstance(aOverview['symbol'], str)
+            self.assertIsInstance(aOverview['reserve'], (int, float))
+            self.assertIsInstance(aOverview['price'], (int, float))
+        builtins.print('\n', overview)
