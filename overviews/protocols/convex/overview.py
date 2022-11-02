95914ba7b07b98df85784ebbb74a5d44d2c09355
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-03 00:39:44 +0500

    Added ConvexStakingPoolAllocationOverview

--- overviews/protocols/convex/overview.py
+++ overviews/protocols/convex/overview.py
@@ -4,6 +4,8 @@ from head.decorators.threadmethod import threadmethod
 from defi.protocols.convex.contracts.BaseRewardPool import ConvexBaseRewardPoolContract
 from defi.protocols.convex.tokens.DepositToken import ConvexDepositTokenContract
 
+from web3 import Web3
+
 
 class ConvexStakingPoolOverview(IInstrumentOverview, ConvexBaseRewardPoolContract):
 
@@ -29,4 +31,32 @@ class ConvexStakingPoolOverview(IInstrumentOverview, ConvexBaseRewardPoolContrac
         }
         overview.append(aOverview)
 
+        return overview
+
+
+class ConvexStakingPoolAllocationOverview(IInstrumentOverview, ConvexBaseRewardPoolContract):
+
+    @threadmethod
+    def getOverview(self, address: str, *args, **kwargs) -> list:
+        overview: list = list()
+
+        address: str = Web3.toChecksumAddress(address)
+
+        tAddress: str = self.stakingToken()
+        t: ConvexDepositTokenContract = ConvexDepositTokenContract()\
+                .setAddress(address=tAddress)\
+                .setProvider(provider=self.provider)\
+                .create()
+        symbol: str = t.symbol()
+        decimals: int = t.decimals()
+        price: float = self.trader.getPrice(major=symbol, vs='USD')
+
+        balanceOf: int = self.balanceOf(address=address)
+
+        allocationOverview: dict = {
+            'symbol': symbol,
+            'amount': balanceOf / 10 ** decimals,
+            'price': price
+        }
+        overview.append(allocationOverview)
         return overview
