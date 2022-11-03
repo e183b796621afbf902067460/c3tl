70524d6106d933609a67e487e138ce69272b0553
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-03 17:20:18 +0500

    Added EllipsisStakingPoolAllocationOverview

--- overviews/protocols/ellipsis/overview.py
+++ overviews/protocols/ellipsis/overview.py
@@ -1,7 +1,61 @@
+from web3 import Web3
+
 from defi.protocols.ellipsis.contracts.Pool import EllipsisPoolContract
+from defi.protocols.ellipsis.tokens.RewardsToken import EllipsisRewardsTokenContract
+from defi.protocols.ellipsis.contracts.EllipsisLPStaking import EllipsisLPStakingContract
+
+from head.interfaces.overview.builder import IInstrumentOverview
+from head.decorators.threadmethod import threadmethod
+from head.bridge.configurator import BridgeConfigurator
+
+from providers.abstracts.fabric import providerAbstractFabric
 
 from overviews.protocols.curve.overview import CurveDEXPoolOverview
 
 
 class EllipsisDEXPoolOverview(CurveDEXPoolOverview, EllipsisPoolContract):
     pass
+
+
+class EllipsisStakingPoolAllocationOverview(IInstrumentOverview, EllipsisRewardsTokenContract):
+    _chiefContract: EllipsisLPStakingContract = EllipsisLPStakingContract
+
+    _providers: dict = {
+        'bsc': {
+            'provider': BridgeConfigurator(
+                            abstractFabric=providerAbstractFabric,
+                            fabricKey='http',
+                            productKey='bsc')\
+                            .produceProduct()
+        }
+    }
+
+    _chiefAddresses: dict = {
+        _providers['bsc']['provider']:
+            {
+                'chief': '0x5B74C99AA2356B4eAa7B85dC486843eDff8Dfdbe'
+            }
+    }
+
+    @threadmethod
+    def getOverview(self, address, *args, **kwargs) -> list:
+        overview: list = list()
+
+        address: str = Web3.toChecksumAddress(address)
+        chief: EllipsisLPStakingContract = self._chiefContract()\
+                .setAddress(address=self._chiefAddresses[self.provider]['chief'])\
+                .setProvider(provider=self.provider)\
+                .create()
+
+        amount: int = chief.userInfo(token=self.address, address=address)[0]
+        decimals: int = self.decimals()
+        symbol: str = self.symbol()
+        price: float = self.trader.getPrice(major=symbol, vs='USD')
+
+        allocationOverview: dict = {
+            'symbol': symbol,
+            'amount': amount / 10 ** decimals,
+            'price': price
+        }
+        overview.append(allocationOverview)
+        return overview
