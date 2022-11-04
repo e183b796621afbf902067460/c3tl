3306f07670fde9f151d481aa5d41e2f0f6822f80
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-04 19:54:57 +0500

    Added TestUniSwapV2DEXPoolAllocationOverview

--- tests/uniswap/tests.py
+++ tests/uniswap/tests.py
@@ -3,7 +3,7 @@ from concurrent.futures import Future
 
 import builtins
 
-from overviews.protocols.uniswap.overview import UniswapV2DEXPoolOverview
+from overviews.protocols.uniswap.overview import UniSwapV2DEXPoolOverview, UniSwapV2DEXPoolAllocationOverview
 from overviews.abstracts.fabric import overviewAbstractFabric
 
 from providers.abstracts.fabric import providerAbstractFabric
@@ -11,7 +11,7 @@ from traders.head.trader import headTrader
 from head.bridge.configurator import BridgeConfigurator
 
 
-class TestUniswapV2DEXPoolOverview(unittest.TestCase):
+class TestUniSwapV2DEXPoolOverview(unittest.TestCase):
 
     _address = '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc'
 
@@ -32,7 +32,7 @@ class TestUniswapV2DEXPoolOverview(unittest.TestCase):
         .create()
 
     def testInstance(self):
-        self.assertIsInstance(self._instance, UniswapV2DEXPoolOverview)
+        self.assertIsInstance(self._instance, UniSwapV2DEXPoolOverview)
 
     def testProvider(self):
         self.assertEqual(self._instance.provider, self._provider)
@@ -55,5 +55,54 @@ class TestUniswapV2DEXPoolOverview(unittest.TestCase):
 
             self.assertIsInstance(aOverview['symbol'], str)
             self.assertIsInstance(aOverview['reserve'], (int, float))
+            self.assertIsInstance(aOverview['price'], (int, float))
+        builtins.print('\n', overview)
+
+
+class TestUniSwapV2DEXPoolAllocationOverview(unittest.TestCase):
+
+    _address = '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc'
+    _wallet = '0xeC08867a12546ccf53b32efB8C23bb26bE0C04f1'
+
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='eth') \
+        .produceProduct()
+
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='dex-pool-allocation-overview',
+        productKey='uniswap') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
+
+    def testInstance(self):
+        self.assertIsInstance(self._instance, UniSwapV2DEXPoolAllocationOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview(address=self._wallet)
+        self.assertIsInstance(future, Future)
+
+        overview = future.result()
+        self.assertIsInstance(overview, list)
+
+        for aOverview in overview:
+            self.assertIsInstance(aOverview, dict)
+
+            self.assertIsInstance(aOverview['symbol'], str)
+            self.assertIsInstance(aOverview['amount'], (int, float))
             self.assertIsInstance(aOverview['price'], (int, float))
         builtins.print('\n', overview)
