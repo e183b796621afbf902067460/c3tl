ef43dd07d0067332f66172ab5a9941672845f7f8
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-04 17:31:46 +0500

    Added TestSushiSwapFarmingPoolAllocationOverview

--- tests/sushiswap/tests.py
+++ tests/sushiswap/tests.py
@@ -3,7 +3,7 @@ from concurrent.futures import Future
 
 import builtins
 
-from overviews.protocols.sushiswap.overview import SushiswapDEXPoolOverview
+from overviews.protocols.sushiswap.overview import SushiSwapDEXPoolOverview, SushiSwapFarmingPoolAllocationOverview
 from overviews.abstracts.fabric import overviewAbstractFabric
 
 from providers.abstracts.fabric import providerAbstractFabric
@@ -11,7 +11,7 @@ from traders.head.trader import headTrader
 from head.bridge.configurator import BridgeConfigurator
 
 
-class TestSushiswapDEXPoolOverview(unittest.TestCase):
+class TestSushiSwapDEXPoolOverview(unittest.TestCase):
 
     _address = '0x611CDe65deA90918c0078ac0400A72B0D25B9bb1'
 
@@ -32,7 +32,7 @@ class TestSushiswapDEXPoolOverview(unittest.TestCase):
         .create()
 
     def testInstance(self):
-        self.assertIsInstance(self._instance, SushiswapDEXPoolOverview)
+        self.assertIsInstance(self._instance, SushiSwapDEXPoolOverview)
 
     def testProvider(self):
         self.assertEqual(self._instance.provider, self._provider)
@@ -55,5 +55,54 @@ class TestSushiswapDEXPoolOverview(unittest.TestCase):
 
             self.assertIsInstance(aOverview['symbol'], str)
             self.assertIsInstance(aOverview['reserve'], (int, float))
+            self.assertIsInstance(aOverview['price'], (int, float))
+        builtins.print('\n', overview)
+
+
+class TestSushiSwapFarmingPoolAllocationOverview(unittest.TestCase):
+
+    _address = '0x05767d9EF41dC40689678fFca0608878fb3dE906'
+    _wallet = '0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50'
+
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='eth') \
+        .produceProduct()
+
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='farming-pool-allocation-overview',
+        productKey='sushiswap') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
+
+    def testInstance(self):
+        self.assertIsInstance(self._instance, SushiSwapFarmingPoolAllocationOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview(address=self._wallet)
+        self.assertIsInstance(future, Future)
+
+        overview = future.result()
+        self.assertIsInstance(overview, list)
+
+        for aOverview in overview:
+            self.assertIsInstance(aOverview, dict)
+
+            self.assertIsInstance(aOverview['symbol'], str)
+            self.assertIsInstance(aOverview['amount'], (int, float))
             self.assertIsInstance(aOverview['price'], (int, float))
         builtins.print('\n', overview)
