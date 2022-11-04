057cf0ae5dd5164e9078538544f12364a55f3086
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-04 22:47:00 +0500

    Added TestGeistLendingPoolAllocationOverview

--- tests/geist/tests.py
+++ tests/geist/tests.py
@@ -3,7 +3,7 @@ from concurrent.futures import Future
 
 import builtins
 
-from overviews.protocols.geist.overview import GeistLendingPoolOverview
+from overviews.protocols.geist.overview import GeistLendingPoolOverview, GeistLendingPoolAllocationOverview
 from overviews.abstracts.fabric import overviewAbstractFabric
 
 from providers.abstracts.fabric import providerAbstractFabric
@@ -58,4 +58,53 @@ class TestGeistLendingPoolOverview(unittest.TestCase):
             self.assertIsInstance(aOverview['price'], (int, float))
             self.assertIsInstance(aOverview['depositAPY'], (int, float))
             self.assertIsInstance(aOverview['borrowAPY'], (int, float))
+        builtins.print('\n', overview)
+
+
+class TestGeistLendingPoolAllocationOverview(unittest.TestCase):
+
+    _address = '0x9FAD24f572045c7869117160A571B2e50b10d068'
+    _wallet = '0xbDfA4f4492dD7b7Cf211209C4791AF8d52BF5c50'
+
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='ftm') \
+        .produceProduct()
+
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='lending-pool-allocation-overview',
+        productKey='geist') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
+
+    def testInstance(self):
+        self.assertIsInstance(self._instance, GeistLendingPoolAllocationOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview(address=self._wallet)
+        self.assertIsInstance(future, Future)
+
+        overview = future.result()
+        self.assertIsInstance(overview, list)
+
+        for aOverview in overview:
+            self.assertIsInstance(aOverview, dict)
+
+            self.assertIsInstance(aOverview['symbol'], str)
+            self.assertIsInstance(aOverview['amount'], (int, float))
+            self.assertIsInstance(aOverview['price'], (int, float))
         builtins.print('\n', overview)
