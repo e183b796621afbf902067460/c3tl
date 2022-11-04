4465d566ef68028ba7bdf4a9362c6914d902c49a
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-05 02:11:50 +0500

    Added TestAaveV2LendingPoolBorrowOverview

--- tests/aave/tests.py
+++ tests/aave/tests.py
@@ -3,7 +3,9 @@ from concurrent.futures import Future
 
 import builtins
 
-from overviews.protocols.aave.overview import AaveV2LendingPoolOverview, AaveV2LendingPoolAllocationOverview
+from overviews.protocols.aave.overview import (
+    AaveV2LendingPoolOverview, AaveV2LendingPoolAllocationOverview, AaveV2LendingPoolBorrowOverview
+)
 from overviews.abstracts.fabric import overviewAbstractFabric
 
 from providers.abstracts.fabric import providerAbstractFabric
@@ -64,7 +66,7 @@ class TestAaveLendingPoolOverview(unittest.TestCase):
 class TestAaveV2LendingPoolAllocationOverview(unittest.TestCase):
 
     _address = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9'
-    _wallet = '0xcb05e41aE2F23A7356e1Cc0eD7db2cd2dC0f9d22'
+    _wallet = '0x7344E478574aCBe6DaC9dE1077430139E17EEc3D'
 
     _provider = BridgeConfigurator(
         abstractFabric=providerAbstractFabric,
@@ -107,4 +109,54 @@ class TestAaveV2LendingPoolAllocationOverview(unittest.TestCase):
             self.assertIsInstance(aOverview['symbol'], str)
             self.assertIsInstance(aOverview['amount'], (int, float))
             self.assertIsInstance(aOverview['price'], (int, float))
+        builtins.print('\n', overview)
+
+
+class TestAaveV2LendingPoolBorrowOverview(unittest.TestCase):
+
+    _address = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9'
+    _wallet = '0x7344E478574aCBe6DaC9dE1077430139E17EEc3D'
+
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='eth') \
+        .produceProduct()
+
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='lending-pool-borrow-overview',
+        productKey='aave') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
+
+    def testInstance(self):
+        self.assertIsInstance(self._instance, AaveV2LendingPoolBorrowOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview(address=self._wallet)
+        self.assertIsInstance(future, Future)
+
+        overview = future.result()
+        self.assertIsInstance(overview, list)
+
+        for aOverview in overview:
+            self.assertIsInstance(aOverview, dict)
+
+            self.assertIsInstance(aOverview['symbol'], str)
+            self.assertIsInstance(aOverview['amount'], (int, float))
+            self.assertIsInstance(aOverview['price'], (int, float))
+            self.assertIsInstance(aOverview['healthFactor'], (int, float))
         builtins.print('\n', overview)
