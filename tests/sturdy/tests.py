b3bb320d6ebb4c5365a79a4ae52eaeb8ba6aab33
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-05 02:52:01 +0500

    Added TestSturdyLendingPoolBorrowOverview

--- tests/sturdy/tests.py
+++ tests/sturdy/tests.py
@@ -3,7 +3,9 @@ from concurrent.futures import Future
 
 import builtins
 
-from overviews.protocols.sturdy.overview import SturdyLendingPoolOverview, SturdyLendingPoolAllocationOverview
+from overviews.protocols.sturdy.overview import (
+    SturdyLendingPoolOverview, SturdyLendingPoolAllocationOverview, SturdyLendingPoolBorrowOverview
+)
 from overviews.abstracts.fabric import overviewAbstractFabric
 
 from providers.abstracts.fabric import providerAbstractFabric
@@ -107,4 +109,54 @@ class TestSturdyLendingPoolAllocationOverview(unittest.TestCase):
             self.assertIsInstance(aOverview['symbol'], str)
             self.assertIsInstance(aOverview['amount'], (int, float))
             self.assertIsInstance(aOverview['price'], (int, float))
+        builtins.print('\n', overview)
+
+
+class TestSturdyLendingPoolBorrowOverview(unittest.TestCase):
+
+    _address = '0xA422CA380bd70EeF876292839222159E41AAEe17'
+    _wallet = '0xB8d5eD3f983EA318FFC9174078a663640139b851'
+
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='eth') \
+        .produceProduct()
+
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='lending-pool-borrow-overview',
+        productKey='sturdy') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
+
+    def testInstance(self):
+        self.assertIsInstance(self._instance, SturdyLendingPoolBorrowOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview(address=self._wallet)
+        self.assertIsInstance(future, Future)
+
+        overview = future.result()
+        self.assertIsInstance(overview, list)
+
+        for aOverview in overview:
+            self.assertIsInstance(aOverview, dict)
+
+            self.assertIsInstance(aOverview['symbol'], str)
+            self.assertIsInstance(aOverview['amount'], (int, float))
+            self.assertIsInstance(aOverview['price'], (int, float))
+            self.assertIsInstance(aOverview['healthFactor'], (int, float))
         builtins.print('\n', overview)
