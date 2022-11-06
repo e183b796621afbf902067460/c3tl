ecb922596e7d2049959d6726bf611058dfdc5620
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-07 03:42:47 +0500

    Added TestCurveFarmingPoolIncentiveOverview

--- tests/curve/tests.py
+++ tests/curve/tests.py
@@ -3,8 +3,10 @@ from concurrent.futures import Future
 
 import builtins
 
-from overviews.protocols.curve.overview import CurveDEXPoolOverview
-from overviews.protocols.curve.overview import CurveFarmingPoolAllocationOverview
+from overviews.protocols.curve.overview import (
+    CurveDEXPoolOverview, CurveFarmingPoolAllocationOverview, CurveFarmingPoolOverview,
+    CurveFarmingPoolIncentiveOverview
+)
 from overviews.abstracts.fabric import overviewAbstractFabric
 
 from providers.abstracts.fabric import providerAbstractFabric
@@ -34,6 +36,54 @@ class TestCurveDEXPoolOverview(unittest.TestCase):
 
     def testInstance(self):
         self.assertIsInstance(self._instance, CurveDEXPoolOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview()
+        self.assertIsInstance(future, Future)
+
+        overview = future.result()
+        self.assertIsInstance(overview, list)
+
+        for aOverview in overview:
+            self.assertIsInstance(aOverview, dict)
+
+            self.assertIsInstance(aOverview['symbol'], str)
+            self.assertIsInstance(aOverview['reserve'], (int, float))
+            self.assertIsInstance(aOverview['price'], (int, float))
+        builtins.print('\n', overview)
+
+
+class TestCurveFarmingPoolOverview(unittest.TestCase):
+
+    _address = '0x72E158d38dbd50A483501c24f792bDAAA3e7D55C'
+
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='eth') \
+        .produceProduct()
+
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='farming-pool-overview',
+        productKey='curve') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
+
+    def testInstance(self):
+        self.assertIsInstance(self._instance, CurveFarmingPoolOverview)
 
     def testProvider(self):
         self.assertEqual(self._instance.provider, self._provider)
@@ -107,3 +157,53 @@ class TestCurveFarmingPoolAllocationOverview(unittest.TestCase):
             self.assertIsInstance(aOverview['amount'], (int, float))
             self.assertIsInstance(aOverview['price'], (int, float))
         builtins.print('\n', overview)
+
+
+class TestCurveFarmingPoolIncentiveOverview(unittest.TestCase):
+
+    _address = '0xd662908ADA2Ea1916B3318327A97eB18aD588b5d'
+    _wallet = '0x905315602ed9a854e325f692ff82f58799beab57'
+
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='eth') \
+        .produceProduct()
+
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='farming-pool-incentive-overview',
+        productKey='curve') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
+
+    def testInstance(self):
+        self.assertIsInstance(self._instance, CurveFarmingPoolIncentiveOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview(address=self._wallet)
+        print(future)
+        # self.assertIsInstance(future, Future)
+        #
+        # overview = future.result()
+        # self.assertIsInstance(overview, list)
+        #
+        # for aOverview in overview:
+        #     self.assertIsInstance(aOverview, dict)
+        #
+        #     self.assertIsInstance(aOverview['symbol'], str)
+        #     self.assertIsInstance(aOverview['amount'], (int, float))
+        #     self.assertIsInstance(aOverview['price'], (int, float))
+        # builtins.print('\n', overview)
