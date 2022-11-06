a2da06b793246fbc4e5ba4bccc3455102b335f99
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-07 00:16:53 +0500

    Added TestConvexStakingPoolIncentiveOverview

--- tests/convex/tests.py
+++ tests/convex/tests.py
@@ -3,7 +3,9 @@ from concurrent.futures import Future
 
 import builtins
 
-from overviews.protocols.convex.overview import ConvexStakingPoolOverview, ConvexStakingPoolAllocationOverview
+from overviews.protocols.convex.overview import (
+    ConvexStakingPoolOverview, ConvexStakingPoolAllocationOverview, ConvexStakingPoolIncentiveOverview
+)
 from overviews.abstracts.fabric import overviewAbstractFabric
 
 from providers.abstracts.fabric import providerAbstractFabric
@@ -106,3 +108,53 @@ class TestConvexStakingPoolAllocationOverview(unittest.TestCase):
             self.assertIsInstance(aOverview['amount'], (int, float))
             self.assertIsInstance(aOverview['price'], (int, float))
         builtins.print('\n', overview)
+
+
+class TestConvexStakingPoolIncentiveOverview(unittest.TestCase):
+
+    _address = '0xB900EF131301B307dB5eFcbed9DBb50A3e209B2e'
+    _wallet = '0xcb05e41aE2F23A7356e1Cc0eD7db2cd2dC0f9d22'
+
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='eth') \
+        .produceProduct()
+
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='staking-pool-incentive-overview',
+        productKey='convex') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
+
+    def testInstance(self):
+        self.assertIsInstance(self._instance, ConvexStakingPoolIncentiveOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview(address=self._wallet)
+        print(future)
+        # self.assertIsInstance(future, Future)
+        #
+        # overview = future.result()
+        # self.assertIsInstance(overview, list)
+        #
+        # for aOverview in overview:
+        #     self.assertIsInstance(aOverview, dict)
+        #
+        #     self.assertIsInstance(aOverview['symbol'], str)
+        #     self.assertIsInstance(aOverview['amount'], (int, float))
+        #     self.assertIsInstance(aOverview['price'], (int, float))
+        # builtins.print('\n', overview)
