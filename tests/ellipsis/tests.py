3a2ae23fcdd8280cec65caee7e2299dd67dd991b
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-07 22:09:10 +0500

    Added TestEllipsisFarmingPoolIncentiveOverview

--- tests/ellipsis/tests.py
+++ tests/ellipsis/tests.py
@@ -4,7 +4,8 @@ from concurrent.futures import Future
 import builtins
 
 from overviews.protocols.ellipsis.overview import (
-    EllipsisDEXPoolOverview, EllipsisFarmingPoolAllocationOverview, EllipsisFarmingPoolOverview
+    EllipsisDEXPoolOverview, EllipsisFarmingPoolAllocationOverview, EllipsisFarmingPoolOverview,
+    EllipsisFarmingPoolIncentiveOverview
 )
 from overviews.abstracts.fabric import overviewAbstractFabric
 
@@ -132,6 +133,55 @@ class TestEllipsisFarmingPoolAllocationOverview(unittest.TestCase):
 
     def testInstance(self):
         self.assertIsInstance(self._instance, EllipsisFarmingPoolAllocationOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview(address=self._wallet)
+        self.assertIsInstance(future, Future)
+
+        overview = future.result()
+        self.assertIsInstance(overview, list)
+
+        for aOverview in overview:
+            self.assertIsInstance(aOverview, dict)
+
+            self.assertIsInstance(aOverview['symbol'], str)
+            self.assertIsInstance(aOverview['amount'], (int, float))
+            self.assertIsInstance(aOverview['price'], (int, float))
+        builtins.print('\n', overview)
+
+
+class TestEllipsisFarmingPoolIncentiveOverview(unittest.TestCase):
+
+    _address = '0xB343F4cDE5e2049857898E800CD385247e21836D'
+    _wallet = '0x3DdfA8eC3052539b6C9549F12cEA2C295cfF5296'
+
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='bsc') \
+        .produceProduct()
+
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='farming-pool-incentive-overview',
+        productKey='ellipsis') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
+
+    def testInstance(self):
+        self.assertIsInstance(self._instance, EllipsisFarmingPoolIncentiveOverview)
 
     def testProvider(self):
         self.assertEqual(self._instance.provider, self._provider)
