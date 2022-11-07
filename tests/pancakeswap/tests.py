f4f526df6435a87f93de1db92725823ef01d4881
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-07 22:34:03 +0500

    Added TestPancakeSwapFarmingPoolIncentiveOverview

--- tests/pancakeswap/tests.py
+++ tests/pancakeswap/tests.py
@@ -4,7 +4,8 @@ from concurrent.futures import Future
 import builtins
 
 from overviews.protocols.pancakeswap.overview import (
-    PancakeSwapDEXPoolOverview, PancakeSwapFarmingPoolAllocationOverview, PancakeSwapFarmingPoolOverview
+    PancakeSwapDEXPoolOverview, PancakeSwapFarmingPoolAllocationOverview, PancakeSwapFarmingPoolOverview,
+    PancakeSwapFarmingPoolIncentiveOverview
 )
 from overviews.abstracts.fabric import overviewAbstractFabric
 
@@ -132,6 +133,55 @@ class TestPancakeSwapFarmingPoolAllocationOverview(unittest.TestCase):
 
     def testInstance(self):
         self.assertIsInstance(self._instance, PancakeSwapFarmingPoolAllocationOverview)
+
+    def testProvider(self):
+        self.assertEqual(self._instance.provider, self._provider)
+
+    def testAddress(self):
+        self.assertEqual(self._instance.address, self._address)
+
+    def testHead(self):
+        self.assertEqual(self._instance.trader, headTrader)
+
+    def test_getOverview(self):
+        future = self._instance.getOverview(address=self._wallet)
+        self.assertIsInstance(future, Future)
+
+        overview = future.result()
+        self.assertIsInstance(overview, list)
+
+        for aOverview in overview:
+            self.assertIsInstance(aOverview, dict)
+
+            self.assertIsInstance(aOverview['symbol'], str)
+            self.assertIsInstance(aOverview['amount'], (int, float))
+            self.assertIsInstance(aOverview['price'], (int, float))
+        builtins.print('\n', overview)
+
+
+class TestPancakeSwapFarmingPoolIncentiveOverview(unittest.TestCase):
+
+    _address = '0x2354ef4DF11afacb85a5C7f98B624072ECcddbB1'
+    _wallet = '0xD183F2BBF8b28d9fec8367cb06FE72B88778C86B'
+
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='bsc') \
+        .produceProduct()
+
+    _instance = BridgeConfigurator(
+        abstractFabric=overviewAbstractFabric,
+        fabricKey='farming-pool-incentive-overview',
+        productKey='pancakeswap') \
+        .produceProduct()() \
+        .setAddress(address=_address) \
+        .setProvider(provider=_provider) \
+        .setTrader(trader=headTrader) \
+        .create()
+
+    def testInstance(self):
+        self.assertIsInstance(self._instance, PancakeSwapFarmingPoolIncentiveOverview)
 
     def testProvider(self):
         self.assertEqual(self._instance.provider, self._provider)
