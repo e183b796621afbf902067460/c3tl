e6cbeba21f5c1a85a653b81ef3bd4a8ccddd0d4b
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-01-24 04:39:39 +0500

    Added TestBinanceUSDTmWholeMarketTradesHandler

--- tests/whole_market_trades_history/binance/tests.py
+++ tests/whole_market_trades_history/binance/tests.py
@@ -3,7 +3,7 @@ import unittest
 import builtins
 from datetime import datetime, timezone, timedelta
 
-from c3tl.handlers.whole_market_trades_history.binance.handlers import BinanceSpotWholeMarketTradesHandler
+from c3tl.handlers.whole_market_trades_history.binance.handlers import BinanceSpotWholeMarketTradesHandler, BinanceUSDTmWholeMarketTradesHandler
 from c3tl.abstract.fabric import c3Abstract
 from c3tl.bridge.configurator import C3BridgeConfigurator
 
@@ -19,6 +19,30 @@ class TestBinanceSpotWholeMarketTradesHandler(unittest.TestCase):
 
     def testInstance(self):
         self.assertIsInstance(self.handler, BinanceSpotWholeMarketTradesHandler)
+
+    def test_get_overview(self):
+        end_time = datetime.utcnow()
+        start_time = end_time - timedelta(minutes=5)
+
+        overview = self.handler.get_overview(
+            ticker='ETHUSDT',
+            start=start_time,
+            end=end_time
+        )
+        builtins.print('\n', overview)
+
+
+class TestBinanceUSDTmWholeMarketTradesHandler(unittest.TestCase):
+
+    product = C3BridgeConfigurator(
+        abstract=c3Abstract,
+        fabric_name='whole_market_trades_history',
+        handler_name='binance_usdt_m'
+    ).produce_handler()
+    handler = product()
+
+    def testInstance(self):
+        self.assertIsInstance(self.handler, BinanceUSDTmWholeMarketTradesHandler)
 
     def test_get_overview(self):
         end_time = datetime.utcnow()
